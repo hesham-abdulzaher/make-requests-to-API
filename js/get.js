0a69726f86e9acc83678049ffd5b851396123d0f
Hesham Abdulzaher <heshamabdulzaher@gmail.com>
2018-02-11 10:30:00 +0200

    I did delete user function by fetch

--- js/get.js
+++ js/get.js
@@ -1,34 +1,40 @@
-// Important Notes && Question
-// When I meet eslam We have talk about *validation*
-// make any svg pen together
-// How many values number that accept return .... done
-
-
-// https://jsonplaceholder.typicode.com/users
-
 fetch('https://bala7a-api.herokuapp.com/users/all') // Step #0
   .then(function(response){ // Step #1
     return response.json()
   })
   .then(function(finalData){ // Step #2
-    // Make logic
-    // console.log(finalData.length)
+
     for(var i = 0; i < finalData.length; i++){
       // Creat Cards to display information about users
-      var container = document.getElementById('container') // Get My container
-      var card = document.createElement('div'); // Creat a new Div in container
-      card.classList = 'card'; // Add Class card
-      container.appendChild(card); // Append in Container
+      var container = document.getElementById('container')
+      var card = document.createElement('div');
+      card.classList = 'card';
+      container.appendChild(card);
 
-      var infoDiv = document.createElement('div'); // Creat a new Div in card
-      infoDiv.classList = 'info'; // Add Class info
-      card.appendChild(infoDiv); // Append in Container
-
+      // Creat div.info in card to show users data
+      var infoDiv = document.createElement('div');
+      infoDiv.classList = 'info';
+      card.appendChild(infoDiv);
+      // Put this information inside div.info
       infoDiv.innerHTML = "Name : " + finalData[i].name + "<br>"
       infoDiv.innerHTML += "Email : " + finalData[i].email + "<br>"
-      // infoDiv.innerHTML += "Password : " + finalData[i].password + "<br>"
 
+      // Creat deleteIcon to delete user
+      var deleteIcon = document.createElement('i');
+      deleteIcon.classList = 'ion-android-delete deleteIcon';
+      card.appendChild(deleteIcon);
+      var usersId = finalData[i]._id
+      deleteIcon.setAttribute('data-id', usersId)
     }
+    // Loop on cards and listen click event
+    document.querySelectorAll('.deleteIcon').forEach(function(deleteIcon){
+      deleteIcon.addEventListener('click', function(clickDetails){
+        var id = clickDetails.target.getAttribute('data-id')
+        deleteData(id)
+        return id;
+      })
+    })
+
   })
   .catch(function(errorMessage){
     alert('something went worng!!!!')
